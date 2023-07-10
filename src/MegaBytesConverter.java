62039bce2411b214032260be5955c1861f797a87
unknown <zshiva35@gmail.com>
2023-07-10 18:03:15 +0545

    Initial Commit

---
+++ src/MegaBytesConverter.java
@@ -0,0 +1,20 @@
+public class MegaBytesConverter {
+    public static void printMegaBytesAndKiloBytes(int kiloBytes){
+
+        int megabytes = kiloBytes / 1024;
+        int remainingKilobytes = kiloBytes % 1024;
+
+        if(kiloBytes < 0){
+            System.out.println("Invalid Value");
+        }
+        else{
+            System.out.println(kiloBytes +" KB = " + megabytes +" MB and "+ remainingKilobytes +" KB");
+        }
+    }
+
+    public static void main(String[] args) {
+        printMegaBytesAndKiloBytes(5000);
+        printMegaBytesAndKiloBytes(-1);
+        printMegaBytesAndKiloBytes(1024);
+    }
+}
